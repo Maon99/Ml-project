bafc8833d9f07090b7d0d26bd0a8e89cc0c79928
Sitthiphon Tankat <95741712+Maon99@users.noreply.github.com>
2022-12-01 16:29:19 +0700

    Update Project.py

--- MLProject/Project.py
+++ MLProject/Project.py
@@ -2,9 +2,10 @@ import numpy as np
 import cv2
 
 #รายชื่อหมวดหมู่ทั้งหมด เรียงตามลำดับ
-CLASSES = ["BACKGROUND", "AEROPLANE", "BICYCLE", "BIRD",
-	"BOTTLE", "BUS", "CAR", "CAT", "CHAIR", "DININGTABLE",
-	"DOG","MOTORBIKE", "PERSON", "POTTEDPLANT", "TRAIN"]
+CLASSES = ["BACKGROUND", "AEROPLANE", "BICYCLE", "BIRD", "BOAT",
+	"BOTTLE", "BUS", "CAR", "CAT", "CHAIR", "COW", "DININGTABLE",
+	"DOG", "HORSE", "MOTORBIKE", "PERSON", "POTTEDPLANT", "SHEEP",
+	"SOFA", "TRAIN", "TVMONITOR"]
 #สีตัวกรอบที่วาดrandomใหม่ทุกครั้ง
 COLORS = np.random.uniform(0,100, size=(len(CLASSES), 3))
 #โหลดmodelจากแฟ้ม
@@ -17,7 +18,7 @@ while True:
 	ret, frame = cap.read()
 	if ret:
 		(h,w) = frame.shape[:2]
-		#ทำpreprocessingq
+		#ทำpreprocessing
 		blob = cv2.dnn.blobFromImage(frame, 0.007843, (300,300), 127.5)
 		net.setInput(blob)
 		#feedเข้าmodelพร้อมได้ผลลัพธ์ทั้งหมดเก็บมาในตัวแปร detections
@@ -25,13 +26,13 @@ while True:
 
 		for i in np.arange(0, detections.shape[2]):
 			percent = detections[0,0,i,2]
-			#กรองเอาเฉพาะค่าpercentที่สูงกว่า0.5 เพิ่มลดได้ตามต้องการ
+			#กรองเอาเฉพาะค่าpercentที่สูงกว่า0.5 
 			if percent > 0.5:
 				class_index = int(detections[0,0,i,1])
 				box = detections[0,0,i,3:7]*np.array([w,h,w,h])
 				(startX, startY, endX, endY) = box.astype("int")
 
-				#ส่วนตกแต่งสามารถลองแก้กันได้ วาดกรอบและชื่อ
+				#ส่วนตกแต่ง
 				label = "{} [{:.2f}%]".format(CLASSES[class_index], percent*100)
 				cv2.rectangle(frame, (startX, startY), (endX, endY), COLORS[class_index], 2)
 				cv2.rectangle(frame, (startX-1, startY-30), (endX+1, startY), COLORS[class_index], cv2.FILLED)
@@ -42,6 +43,6 @@ while True:
 		if cv2.waitKey(1) & 0xFF==ord('q'):
 			break
 
-
+#หลังเลิกใช้แล้วเคลียร์memoryและปิดกล้อง
 cap.release()
 cv2.destroyAllWindows()
